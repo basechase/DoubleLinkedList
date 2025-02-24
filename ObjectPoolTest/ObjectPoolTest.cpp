f8016eb87623bdfe272a0cdbaa200f905f4a1b5a
basechase <chase7y@gmail.com>
2025-02-24 12:43:16 -0600

    creating new unit tests for objectpool

---
+++ ObjectPoolTest/ObjectPoolTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../DoubleLinkedList/ObjectPool.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ObjectPoolTest
+{
+	TEST_CLASS(ObjectPoolTest)
+	{
+	
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
